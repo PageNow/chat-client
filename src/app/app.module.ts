e5fae572fd2cbdecadecd1a8ff52b579a1df5a03
Yongkyun Lee <ylee2@caltech.edu>
2021-05-04 02:01:50 -0700

    Initial commit with amplify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AmplifyUIAngularModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,6 +54,11 @@
  *
  */
 
+(window as any).global = window;
+(window as any).process = {
+    env: { DEBUG: undefined },
+};
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
