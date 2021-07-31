a898b6ea8889549ad3d6be649dad3f35da2523f0
Yongkyun Lee <yongkyun.daniel.lee@gmail.com>
2021-07-30 23:49:21 -0400

    Added notifications module

--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { NotificationsComponent } from './notifications.component';
+import { NotificationsRoutingModule } from './notifications-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        NotificationsComponent
+    ],
+    imports: [
+        CommonModule,
+        NotificationsRoutingModule
+    ]
 })
 export class NotificationsModule { }
